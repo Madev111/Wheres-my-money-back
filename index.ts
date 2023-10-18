0785a0a4d6a6432c86e66cb93ca1296a4ec0fa50
Mateusz Wawrzyniak <mateusz.wawrzyniak3@gmail.com>
2023-10-18 13:34:54 +0200

    Environment has been prepared.

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express, {json} from 'express';
+// import cors from 'cors';
+import 'express-async-errors';
+import rateLimit from "express-rate-limit";
+
+const app = express();
+
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+// }));
+
+app.use(json());
+app.use(rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 100,
+}));
+
+
+app.listen(3001, '0.0.0.0', ()=> {
+    console.log('Listening on http://localhost:3001')
+})
